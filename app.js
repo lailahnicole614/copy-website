25dcf7750746434408495b16e4c5e8bd0f01523b
Lailah Berry <lailahnicole614@gmail.com>
2023-01-05 16:52:15 -0800

    added image

--- app.js
+++ app.js
@@ -1,11 +1,15 @@
 /* Imports */
-
+const searchInput = document.getElementById('search-input');
+const searchBtn = document.getElementById('search-button');
 /* Get DOM Elements */
 
 /* State */
-
 /* Events */
-
+Display: searchBtn.addEventListener('click'),
+    () => {
+        nameDisplay.textContent = searchInput.value;
+        searchInput.value = '';
+    };
 /* Display Functions */
 
 // (don't forget to call any display functions you want to run on page load!)
